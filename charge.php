31f16a405688b85a93c7c734cd6a0dda39fc387d
Moiz Qureshi <mqureshi@salesforce.com>
2019-04-27 20:52:34 -0700

    modified charge.php

--- charge.php
+++ charge.php
@@ -1,29 +1,18 @@
 <?php
+  require_once('config.php');
 
-var handler = StripeCheckout.configure({
-  key: 'pk_test_Fywng1DvZnhmZtPltht2kt1T',
-  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-  token: function(token) {
-    $("#stripeToken").val(token.id);
-    $("#stripeEmail").val(token.email);
-    $("#myForm").submit();
-  }
-});
+  $token  = $_POST['stripeToken'];
 
-$('#customButton').on('click', function(e) {
-  var amount = $("#amount").val() * 100;
-  var displayAmount = parseFloat(Math.floor($("#amount").val() * 100) / 100).toFixed(2);
-  // Open Checkout with further options
-  handler.open({
-    name: 'Demo Site',
-    description: 'Custom amount ($' + displayAmount + ')',
-    amount: amount
-  });
-  e.preventDefault();
-});
+  $customer = \Stripe\Customer::create(array(
+      'email' => ($_POST['stripeEmail']),
+      'source'  => $token
+  ));
 
-// Close Checkout on page navigation
-$(window).on('popstate', function() {
-  handler.close();
-});
+  $charge = \Stripe\Charge::create(array(
+      'customer' => $customer->id,
+      'amount'   => ($_POST['amount']*100),
+      'currency' => 'usd'
+  ));
+
+echo 'Success! You have been charged $' . ($_POST['amount']*100);
 ?>
